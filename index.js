2319ca77c4a9b880ded25b652a600813dde9f0a6
Manabu Shinsaka <shinsaka@serverworks.co.jp>
2025-07-29 19:14:40 +0900

    猫の動作を追加

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const rl = readline.createInterface({
 });
 
 rl.question('何か入力してください: ', (answer) => {
-  if (answer.toLowerCase() === 'dog') {
+  if (lowerCaseAnswer === 'dog') {
     console.log('ワンワン！');
+  } else if (lowerCaseAnswer === 'cat') {
+    console.log('ニャー！');
   } else {
-    console.log(`"${answer}" ですね。犬の鳴き声ではありません。`);
+    console.log(`"${answer}" ですね。犬や猫の鳴き声ではありません。`);
   }
 
   rl.close();
